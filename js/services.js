caaa5b460748bcab8e195541c459dad21b8342a6
= <cavaleiromiau@gmail.com>
2025-08-02 18:31:09 -0300

    low update

--- js/services.js
+++ js/services.js
@@ -6,6 +6,12 @@
         "image": "img/cut.jpg"
     },
     {
+        "name": "Cutting hair",
+        "description": "Simple cuts at the ends and other corners, but maintaining beauty.",
+        "price": "$ 40",
+        "image": "img/cut.jpg"
+    },
+    {
         "name": "Style hairstyle",
         "description": "Cut, style, blow dry, etc., everything you need for medium to shorter hair..",
         "price": "$ 100",
