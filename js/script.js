aa274f27ed6d7f4cdb64914ad1fc11ede9c1524c
= <cavaleiromiau@gmail.com>
2025-08-06 20:41:55 -0300

    ss

--- js/script.js
+++ js/script.js
@@ -16,3 +16,24 @@
             });
         })
         .catch(error => console.error('Erro ao carregar serviços:', error));
+
+    let scrollContainer = document.querySelector(".gallery");
+    let backBtn = document.getElementById("backBtn")
+    let nextBtn = document.getElementById("nextBtn")
+
+    scrollContainer.addEventListener("wheel", (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+        scrollContainer.style.scrollBehavior = "auto";
+
+    });
+
+    nextBtn.addEventListener("click", ()=> {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft += 900;
+    });
+
+    backBtn.addEventListener("click", ()=> {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft -= 900;
+    });
